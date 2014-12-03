35e481d3650ae265daa7903493b3ec8b9bf391fe
Christian Schreiner (home) <cas@mathoni.net>
2014-12-03 09:10:12 -0700

    adapted to use the pointer arithmetic

--- try/map/three.cpp
+++ try/map/three.cpp
@@ -2,16 +2,19 @@
 #include <iostream>
 #include <map>
 
+int dummy= 17;
+
 int main ( int argc, char* argv[] )
 {{
    std::map<uint8_t*, bool> poisonedMem;
 
-   poisonedMem[(uint8_t*)0]= false;
-   poisonedMem[(uint8_t*)1]= true;
-   poisonedMem[(uint8_t*)2]= true;
-   poisonedMem[(uint8_t*)3]= true;
+   poisonedMem[&dummy+0]= false;
+   poisonedMem[&dummy+1]= true;
+   poisonedMem[&dummy+2]= true;
+   poisonedMem[&dummy+3]= true;
 
-   for ( int ii= 0; ii < 4; ii++ )  {
-      std::cout << ii << " is " << poisonedMem[(uint8_t*)ii] << "\n";
+   // this intentionally fetches elements that haven't been set
+   for ( int ii= 0; ii < 10; ii++ )  {
+      std::cout << ii << " is " << poisonedMem[&dummy+ii] << "\n";
    }
 }}
