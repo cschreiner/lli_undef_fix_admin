47129923e0983977bee57925f1a6839cb9a92aec
Christian Schreiner <cas@cs.utah.edu>
2014-12-02 22:40:04 -0700

    initial version of a first attempt to get C++'s map template to work

---
+++ try/map/one.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <map>
+
+int main ( int argc, char* argv[] )
+{{
+   std::map<int64_t, bool) poisonedMem;
+
+   poisonedMem[0]= false;
+   poisonedMem[1]= true;
+
+   cout << "0 is " << poisonedMem[0] << ", 1 is " << poisonedMem[1];
+}}
