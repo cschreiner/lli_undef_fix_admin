a3ba433504892472642ab441311cc83bb2b3e7af
Christian Schreiner (home) <cas@mathoni.net>
2014-12-03 08:56:35 -0700

    changed to use pointers instead of integers as a key

--- try/map/two.cpp
+++ try/map/two.cpp
@@ -4,14 +4,14 @@
 
 int main ( int argc, char* argv[] )
 {{
-   std::map<int64_t, bool> poisonedMem;
+   std::map<uint8_t*, bool> poisonedMem;
 
-   poisonedMem[0]= false;
-   poisonedMem[1]= true;
-   poisonedMem[2]= true;
-   poisonedMem[3]= true;
+   poisonedMem[(uint8_t*)0]= false;
+   poisonedMem[(uint8_t*)1]= true;
+   poisonedMem[(uint8_t*)2]= true;
+   poisonedMem[(uint8_t*)3]= true;
 
    for ( int ii= 0; ii < 4; ii++ )  {
-      std::cout << ii << " is " << poisonedMem[ii] << "\n";
+      std::cout << ii << " is " << poisonedMem[(uint8_t*)ii] << "\n";
    }
 }}
