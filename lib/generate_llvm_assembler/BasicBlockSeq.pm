978f822b5fe095b1384f1b656b6761d14762ef64
Christian Schreiner <cas@cs.utah.edu>
2015-04-07 18:20:08 -0600

    fixed call to a function's old name

--- lib/generate_llvm_assembler/BasicBlockSeq.pm
+++ lib/generate_llvm_assembler/BasicBlockSeq.pm
@@ -246,7 +246,7 @@ sub generate
    }
 
    if ( $this->{'opt_hashref'}->{'startPoison'} )  {
-     $instructions.= "  " . reg_context::getName() . 
+     $instructions.= "  " . $this->getRegName() . 
 	   "= sub nuw " . $this->regWidth()->getName() . " 0, 1 ; generates POISON \n";
      # TODO2: replace the above operands with random numbers
    }
