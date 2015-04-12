05dd5f5a78751ab0a1805b7464ca7f43578d899a
Christian Schreiner (home) <cas@mathoni.net>
2015-04-11 18:41:08 -0600

    adding a class template for every PERL package

---
+++ src/generate_llvm_ir/AddrName.java
@@ -0,0 +1,187 @@
+/*.. 
+   * Project Name: lli_undef_fix
+   *
+   * File Name: AddrName.java
+   *
+   * File Description: class AddrName
+   *	(list here the package-scope classes in this file)
+   *
+   * lli_undef_fix was written by Christian A. Schreiner at University of
+   * Utah.  Copyright (C) 2015-2015 by University of Utah.  All rights
+   * reserved.  You may use, examine, or modify this file only in accordance
+   * with the GNU Public License, or, alternately, by special written
+   * arrangement with the author.  This file comes with no warranties.  If
+   * you use it and something breaks, you are solely responsible for
+   * cleaning up afterwards.
+   *
+   */
+
+/*.. Configuration Management Information:
+   * 
+   * $Source: /home/cas/templates/auto_uut/RCS/cas_tmpl.java,v $
+   * $File: $
+   * $Author: cas $
+   * $Date: 2014/04/04 10:25:56 $
+   * $Revision: 1.3 $
+   * 
+   */
+
+/* ****************************************************************************
+   *   package
+   * **************************************************************************
+   */
+package <$package_name>;
+
+/* ****************************************************************************
+   *   imports
+   * **************************************************************************
+   */
+//import java.util.*;
+//import java.applet.Applet;
+//import java.awt.*;
+//import java.awt.event.*;
+//import java.awt.Color.*;
+//import java.awt.geom.*;
+
+
+// ****************************************************************************
+// File's primary class: AddrName
+// ****************************************************************************
+/*** 
+   * <ul>
+   * <li> Description: 
+   *
+   * <li> Algorithm: 
+   * </ul>
+   */
+public class AddrName 
+{
+
+/* ############################################################################
+   * primary class' direct contents
+   * ##########################################################################
+   */
+
+   /* =========================================================================
+      * class variables
+      * =======================================================================
+      */
+
+   /* =========================================================================
+      * instance variables
+      * =======================================================================
+      */
+
+
+   /* =========================================================================
+      * constructors
+      * =======================================================================
+      */
+
+   // -------------------------------------------------------------------------
+   // AddrName()
+   // -------------------------------------------------------------------------
+   /*** default constructor
+      *
+      * <ul>
+      * <li> Detailed Description: 
+      *
+      * <li> Algorithm: 
+      *
+      * <li> Reentrancy: unknown
+      *
+      * <li> No inputs.
+      * </ul>
+      * 
+      * @return - n/a (it's a constructor!)
+      *
+      * @throws
+      */
+   private AddrName()
+   {{
+      System.err.println ( "Internal error: "+
+	    "unexpected call to default constructor for AddrName." );
+      System.exit(-127);
+   }}
+
+
+   /* =========================================================================
+      * methods
+      * =======================================================================
+      */
+
+
+/* ############################################################################
+   * trivially simple subclasses
+   * ##########################################################################
+   */
+
+/* ****************************************************************************
+   * end of primary class
+   * **************************************************************************
+   */
+} // end class AddrName
+
+
+/* ****************************************************************************
+   * templates 
+   * **************************************************************************
+   */
+
+/* template is 22 lines long */
+// ############################################################################
+// class_name()
+// ############################################################################
+/***  
+   * 
+   * <ul>
+   * 
+   * <li> Detailed Description: 
+   * 
+   * <li> Algorithm: 
+   * </ul>
+   * 
+   */
+//private class class_name
+//{
+//   /* variables */
+//
+//   /* constructors */
+//   class_name() { }
+//
+//   /* methods */
+//} // end nestedclass class_name
+
+
+   /* template is 22 lines long */
+   /* Note: change the initial dots to stars for public functions */
+   // ------------------------------------------------------------------------
+   // fname()
+   // ------------------------------------------------------------------------
+   /***  
+      * 
+      * <ul>
+      * <li> Detailed Description: 
+      *
+      * <li> Algorithm: 
+      *
+      * <li> Reentrancy: unknown
+      *
+      * <li> No inputs.
+      * </ul>
+      * 
+      * @return - 
+      *
+      * @throws 
+      */
+   //private type fname()
+   //{{
+   //}}
+
+
+
+/* ****************************************************************************
+   *   end of file
+   * **************************************************************************
+   */
+
