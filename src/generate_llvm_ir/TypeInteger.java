e837b60340ed220e42b5b0d07c49647c15da2d92
Christian Schreiner (home) <cas@mathoni.net>
2015-04-24 12:53:27 -0600

    finished converting getRandShiftVal()

--- src/generate_llvm_ir/TypeInteger.java
+++ src/generate_llvm_ir/TypeInteger.java
@@ -188,6 +188,180 @@ public class TypeInteger
       return;
    }}
 
+   // ------------------------------------------------------------------------
+   // getRandVal()
+   // ------------------------------------------------------------------------
+   /**  Gets a random value for this size of integer, using the full range 
+    *	of the possible integer values.
+    * 
+    * <ul>
+    * <li> Detailed Description: 
+    *
+    * <li> Algorithm: 
+    *
+    * <li> Reentrancy: unknown
+    *
+    * <li> No inputs.
+    * </ul>
+    * 
+    * @return - per description
+    *
+    * @throws 
+    */
+   public long getRandVal()
+   {{
+      return getRandVal(0);
+   }}
+
+   // ------------------------------------------------------------------------
+   // getRandVal(int)
+   // ------------------------------------------------------------------------
+   /**  returns a random value for this size of integer
+    * 
+    * <ul>
+    * <li> Detailed Description: 
+    *
+    * <li> Algorithm: 
+    *
+    * <li> Reentrancy: unknown
+    *
+    * </ul>
+    *
+    * @param margin - 
+    *	Do not return a value within this distance of the min or max value.
+	Often specified as 0.  This guarantees that the value returned can be 
+	incremented or decremented by up to this amount without overflow.
+    * 
+    * @return - 
+    *
+    * @throws 
+    */
+   public long getRandVal( int margin )
+   {{
+      long range= maxVal- minVal- 2* margin;
+
+      if ( 0 ) {
+	 System.out.print( "max=\""+ maxVal+ "\", min=\""+ minVal+
+			   "\", margin=\""+ margin+ "\"\n" );
+	 System.out.print( "   range=\""+ range+ "\"\n" );
+      }
+
+      long val= randomizer.nextInt( range )+ minVal+ margin;
+      return val;
+   }}
+
+   // ------------------------------------------------------------------------
+   // getRandShiftVal()
+   // ------------------------------------------------------------------------
+   /** Returns a random number from 0 to this instance's bitwidth. 
+    * This is the legal range that a register may be shifted right or left.
+    * 
+    * <ul>
+    * <li> Detailed Description: 
+    *	Strange, shifting by the width of the register is sometimes allowed 
+    *	and sometimes not.  For example, 
+    *	   %9= lshr i13 %8, 13
+    *	   %11= ashr  i13 %10, 13
+    *	are ok, but
+    *	   %9= lshr exact i13 %8, 13
+    *	and
+    *	   %11= ashr exact i13 %10, 13
+    *	are not.  Even though the 'exact' flag is supposted to generate
+    *	poison if 0 bits are shifted out, its presence causes
+    *	execution to halt with an "overshift" error.  Officially, shifting by 
+    *	the register width or larger yields an undefined result (NOT undefined 
+    *	behavior). Is the fact that this is sometimes allowed and sometimes 
+    *	not an error in the interpreter? TODO2: look into this.  If
+    *	the result is undefined, I assume the interpreter should replaced it 
+    *	with a random value of appropriate width.
+    *
+    * <li> Algorithm: 
+    *
+    * <li> Reentrancy: unknown
+    *
+    * <li> No inputs.
+    * </ul>
+    * 
+    * @return per description
+    *
+    * @throws 
+    */
+   public int getRandShiftVal()
+   {{
+      int val= randomizer( bitWidth );
+      return val;
+   }}
+
+## ===========================================================================
+## Subroutine compareTo()
+## ===========================================================================
+# Description: compares this instance to another one
+#
+# Method: 
+#
+# Notes: 
+#
+# Warnings: 
+#
+# Inputs: 
+#   $this: the primary instance to act on (provided by PERL)
+#   $that: the other instance to compare
+# 
+# Outputs: 
+#   
+# Return Value: 
+#   -1 if this < that
+#   0 if this == that
+#   1 if this > that
+#
+# ============================================================================
+sub compareTo
+{{
+   my( $this, $that )= @_;
+
+   # TODO3: if we add non-integer types in the future, add something somewhere
+   # to insure both instances being compared are integers.
+
+   if ( ref($that) ne "TypeInteger" )  {
+      confess ( $main::scriptname . ": internal error 2015apr10_095356, " . 
+	    "code=\"" . ref($that) . "\"\n" );
+   }
+
+   return $this->getBitwidth() <=> $that->getBitwidth();
+}}
+
+## ===========================================================================
+## Short get subroutines
+## ===========================================================================
+# Description: short subroutines that only get one field
+#
+# Inputs: $perl_this: the instance in question (provided by PERL)
+#
+# Return Value: the field's value
+#
+# ============================================================================
+#sub sub_name {{ my($perl_this)= @_; return $perl_this->{'xx'}; }}
+sub getBitwidth {{ my($perl_this)= @_; return $perl_this->{'bitwidth'}; }}
+sub getMaxVal {{ my($perl_this)= @_; return $perl_this->{'maxVal'}; }}
+sub getMinVal {{ my($perl_this)= @_; return $perl_this->{'minVal'}; }}
+sub getName {{ my($perl_this)= @_; return $perl_this->{'name'}; }}
+
+## ===========================================================================
+## Short set subroutines
+## ===========================================================================
+# Description: short subroutines that only set one field
+#
+# Inputs: the field's new value
+#
+# Return Value: none
+#
+# ============================================================================
+#sub sub_name
+#{{
+#   my ($ii)= @_;
+#}}
+
+
 
 
 /* ############################################################################
