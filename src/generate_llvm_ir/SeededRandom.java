320331fb2a71f2549718b53825f3ae5151d27eee
Christian Schreiner (home) <cas@mathoni.net>
2015-04-27 19:56:26 -0600

    added initial functionality

--- src/generate_llvm_ir/SeededRandom.java
+++ src/generate_llvm_ir/SeededRandom.java
@@ -30,13 +30,13 @@
    *   package
    * **************************************************************************
    */
-package <$package_name>;
+package generate_llvm_ir;
 
 /* ****************************************************************************
    *   imports
    * **************************************************************************
    */
-//import java.util.*;
+import java.util.*;
 //import java.applet.Applet;
 //import java.awt.*;
 //import java.awt.event.*;
@@ -47,13 +47,15 @@ package <$package_name>;
 // ****************************************************************************
 // File's primary class: SeededRandom
 // ****************************************************************************
-/*** 
-   * <ul>
-   * <li> Description: 
-   *
-   * <li> Algorithm: 
-   * </ul>
-   */
+/*** creates random numbers from a seed
+ *
+ * <ul>
+ * <li> Description: 
+ *
+ * <li> Algorithm: nextVal= current * a constant + a constant.
+ *	The implementation could use some tightening up.
+ * </ul>
+ */
 public class SeededRandom 
 {
 
@@ -66,11 +68,16 @@ public class SeededRandom
       * class variables
       * =======================================================================
       */
+   // instance for everyone to use
+   public static SeededRandom x= new SeededRandom();  
+
 
    /* =========================================================================
       * instance variables
       * =======================================================================
       */
+   private int seed;
+   private int currentVal;
 
 
    /* =========================================================================
@@ -97,11 +104,22 @@ public class SeededRandom
       *
       * @throws
       */
-   private SeededRandom()
+   public SeededRandom()
    {{
-      System.err.println ( "Internal error: "+
-	    "unexpected call to default constructor for SeededRandom." );
-      System.exit(-127);
+      //System.err.println ( "Internal error: "+
+      //   "unexpected call to default constructor for SeededRandom." );
+      //System.exit(-127);
+
+      String envSeed= System.getenv( "SEED" );
+      if ( envSeed == null ) {
+	 Random randomizer= new Random();
+	 seed= randomizer.nextInt();
+      } else {
+	 seed= Integer.ParseInt( envSeed );
+      }
+      currentVal= seed;
+
+      next();
    }}
 
 
@@ -109,6 +127,127 @@ public class SeededRandom
       * methods
       * =======================================================================
       */
+
+   // ------------------------------------------------------------------------
+   // next()
+   // ------------------------------------------------------------------------
+   /**  computes the next random number
+    * 
+    * <ul>
+    * <li> Detailed Description: 
+    *
+    * <li> Algorithm: 
+    *
+    * <li> Reentrancy: unknown
+    *
+    * <li> No inputs.
+    * </ul>
+    * 
+    * @return - void
+    *
+    * @throws 
+    */
+   private void next()
+   {{
+      long tmp= currentVal* 260726541+ 1195963154;
+      currentVal= tmp & 0xffffffff;
+   }}
+
+   // ------------------------------------------------------------------------
+   // nextInt()
+   // ------------------------------------------------------------------------
+   /**  gets the next integer from this random number generator
+    * 
+    * <ul>
+    * <li> Detailed Description: 
+    *
+    * <li> Algorithm: 
+    *
+    * <li> Reentrancy: unknown
+    *
+    * </ul>
+    *
+    * @param max - the (max value+1) for the random number
+    * 
+    * @return a random integer such that 0 <= n < max 
+    *
+    * @throws 
+    */
+   public int nextInt( int max )
+   {{
+      next();
+
+      // TODO2: make this more uniform, in case currentVal+ max > INT_MAX
+      return currentVal % max; 
+   }}
+
+   // ------------------------------------------------------------------------
+   // nextLong()
+   // ------------------------------------------------------------------------
+   /**  gets the next integer from this random number generator
+    * 
+    * <ul>
+    * <li> Detailed Description: 
+    *
+    * <li> Algorithm: 
+    *
+    * <li> Reentrancy: unknown
+    *
+    * </ul>
+    *
+    * @param max - the (max value+1) for the random number
+    * 
+    * @return a random integer such that 0 <= n < max 
+    *
+    * @throws 
+    */
+   public int nextLong( long max )
+   {{
+      next();
+      long upper= currentVal;
+      next();
+      long lower= currentVal;
+      long longVal= (upper << 32)+ lower;
+
+      // TODO2: make this more uniform, in case currentVal+ max > INT_MAX
+      return longVal % max; 
+   }}
+
+   // ------------------------------------------------------------------------
+   // nextDouble()
+   // ------------------------------------------------------------------------
+   /** generates a random double between 0 and 1 
+    * 
+    * <ul>
+    * <li> Detailed Description: 
+    *
+    * <li> Algorithm: 
+    *
+    * <li> Reentrancy: unknown
+    *
+    * <li> No inputs.
+    * </ul>
+    * 
+    * @return a double d such that 0 <= d < 1
+    *
+    * @throws 
+    */
+   public double nextDouble()
+   {{
+      next();
+
+      // TODO2: see if there is a way to do this with longs for more
+      // "randomness".
+      double retVal= currentVal / Integer.MAX_VALUE;
+      while ( retVal < 0 ) {
+	 retVal+= 1.0;
+      }
+      while ( retVal > 0 ) {
+	 retVal-= 1.0;
+      }
+      return retVal;
+   }}
+
 
 
 /* ############################################################################
@@ -158,22 +297,22 @@ public class SeededRandom
    // ------------------------------------------------------------------------
    // fname()
    // ------------------------------------------------------------------------
-   /***  
-      * 
-      * <ul>
-      * <li> Detailed Description: 
-      *
-      * <li> Algorithm: 
-      *
-      * <li> Reentrancy: unknown
-      *
-      * <li> No inputs.
-      * </ul>
-      * 
-      * @return - 
-      *
-      * @throws 
-      */
+   /**  
+    * 
+    * <ul>
+    * <li> Detailed Description: 
+    *
+    * <li> Algorithm: 
+    *
+    * <li> Reentrancy: unknown
+    *
+    * <li> No inputs.
+    * </ul>
+    * 
+    * @return - 
+    *
+    * @throws 
+    */
    //private type fname()
    //{{
    //}}
