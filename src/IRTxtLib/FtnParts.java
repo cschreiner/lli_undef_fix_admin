055d95cbca73a45de056c5cce82561692d3020db
Christian Schreiner (home) <cas@mathoni.net>
2015-05-20 02:49:46 -0600

    adding a class that splits a function into its constituent parts

---
+++ src/IRTxtLib/FtnParts.java
@@ -0,0 +1,186 @@
+/* 
+ * Program Name: lli_undef_fix
+ *
+ * File Name: FtnParts.java
+ *
+ * File Description: class FtnParts
+ *	(list here the package-scope classes in this file)
+ *
+ * lli_undef_fix was written by Christian A. Schreiner at University of
+ * Utah.  Copyright (C) 2015-2015 by University of Utah.  All rights
+ * reserved.  You may use, examine, or modify this file only in accordance
+ * with the GNU Public License, or, alternately, by special written
+ * arrangement with the author.  This file comes with no warranties.  If
+ * you use it and something breaks, you are solely responsible for
+ * cleaning up afterwards.
+ */
+
+/*.. Configuration Management Information:
+   * 
+   * $Source: /net/home/cas/templates/auto_uut/RCS/cas_tmpl.java,v $
+   * $File: $
+   * $Author: cas $
+   * $Date: 2015/05/20 07:15:19 $
+   * $Revision: 1.4 $
+   * 
+   */
+
+/* ****************************************************************************
+   *   package
+   * **************************************************************************
+   */
+package <$package_name>;
+
+/* ****************************************************************************
+ *   imports
+ * ****************************************************************************
+ */
+//import java.util.*;
+//import java.applet.Applet;
+//import java.awt.*;
+//import java.awt.event.*;
+//import java.awt.Color.*;
+//import java.awt.geom.*;
+
+
+// ****************************************************************************
+// File's primary class: FtnParts
+// ****************************************************************************
+/** 
+ * <ul>
+ * <li> Description: 
+ *
+ * <li> Algorithm: 
+ * </ul>
+ */
+public class FtnParts 
+{
+
+/* ############################################################################
+ * primary class' direct contents
+ * ############################################################################
+ */
+   
+   /* =========================================================================
+    * class variables
+    * =========================================================================
+    */
+
+   /* =========================================================================
+    * instance variables
+    * =========================================================================
+    */
+
+
+   /* =========================================================================
+    * constructors
+    * =========================================================================
+    */
+
+   // -------------------------------------------------------------------------
+   // FtnParts()
+   // -------------------------------------------------------------------------
+   /** default constructor
+    *
+    * <ul>
+    * <li> Detailed Description: 
+    *
+    * <li> Algorithm: 
+    *
+    * <li> Reentrancy: unknown
+    *
+    * <li> No inputs.
+    * </ul>
+    * 
+    * @return - n/a (it's a constructor!)
+    *
+    * @throws
+    */
+   private FtnParts()
+   {{
+      System.err.println ( "Internal error: "+
+	    "unexpected call to default constructor for FtnParts." );
+      System.exit(-127);
+   }}
+
+
+   /* =========================================================================
+    * methods
+    * =========================================================================
+    */
+
+
+/* ############################################################################
+ * trivially simple subclasses
+ * ############################################################################
+ */
+
+/* ****************************************************************************
+ * end of primary class
+ * ****************************************************************************
+ */
+} // end class FtnParts
+
+
+/* ****************************************************************************
+ * templates 
+ * ****************************************************************************
+ */
+
+/* template is 22 lines long */
+// ############################################################################
+// class_name()
+// ############################################################################
+/**  
+ * 
+ * <ul>
+ * 
+ * <li> Detailed Description: 
+ * 
+ * <li> Algorithm: 
+ * </ul>
+ * 
+ */
+//private class class_name
+//{
+//   /* variables */
+//
+//   /* constructors */
+//   class_name() { }
+//
+//   /* methods */
+//} // end nestedclass class_name
+
+
+   /* template is 22 lines long */
+   /* Note: change the initial dots to stars for public functions */
+   // ------------------------------------------------------------------------
+   // fname()
+   // ------------------------------------------------------------------------
+   /**  
+    * 
+    * <ul>
+    * <li> Detailed Description: 
+    *
+    * <li> Algorithm: 
+    *
+    * <li> Reentrancy: unknown
+    *
+    * <li> No inputs.
+    * </ul>
+    * 
+    * @return - 
+    *
+    * @throws 
+    */
+   //private type fname()
+   //{{
+   //}}
+
+
+
+/* ****************************************************************************
+ *   end of file
+ * ****************************************************************************
+ */
+
