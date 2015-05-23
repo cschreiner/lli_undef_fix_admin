37e8ac298fa69af3a2311da4ea635ba8c7de4ee8
Christian Schreiner (home) <cas@mathoni.net>
2015-05-23 02:48:23 -0600

    improved output, set repeatability

--- test/cases/ir_ftn_mktest..basic.sh
+++ test/cases/ir_ftn_mktest..basic.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 # basic test of ir_ftn_mktest
 
-$PROJ_ROOT/admin/bin/ir_ftn_mktest $PROJ_ROOT/test/data/funcs-3-2/000.ll linus
+export SEED=1 # make sure the random number generator works repeatably
+
+$PROJ_ROOT/admin/bin/ir_ftn_mktest \
+      $PROJ_ROOT/admin/test/data/funcs-3-2/000.ll linus
 
 for file in `find linus -type f | sort `; do
+   echo '#################################################################'
    echo '#################################################################'
    echo file $file:
    echo " "
    cat $file
    echo " "
    echo '#################################################################'
+   echo '#################################################################'
 done
 echo " "
-echo '(end of output')
+echo '(end of output)'
 
