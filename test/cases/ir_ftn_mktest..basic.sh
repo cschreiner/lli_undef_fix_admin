ff1789b8562256d757def642b713d5bb2417a2fe
Christian Schreiner (home) <cas@mathoni.net>
2015-05-23 02:52:59 -0600

    added ability to run outside of testdriver if needed

--- test/cases/ir_ftn_mktest..basic.sh
+++ test/cases/ir_ftn_mktest..basic.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 # basic test of ir_ftn_mktest
 
+scriptname=ir_ftn_mktest..basic.sh
+
+if [ -z "$TESTDRIVER_CASE_WORKDIR" ]; then
+   TESTDRIVER_CASE_WORKDIR=tmp/${scriptname}_`date +%Y%b%d_%H%M%S`_$$
+   mkdir $TESTDRIVER_CASE_WORKDIR
+   echo using TESTDRIVER_CASE_WORKDIR=\"$TESTDRIVER_CASE_WORKDIR\"
+fi
+
 export SEED=1 # make sure the random number generator works repeatably
+
+cd $TESTDRIVER_CASE_WORKDIR
 
 $PROJ_ROOT/admin/bin/ir_ftn_mktest \
       $PROJ_ROOT/admin/test/data/funcs-3-2/000.ll linus
