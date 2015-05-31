24d393cfbb62bb0db16df5a1ee33aad604bcff02
Christian Schreiner <cas@cs.utah.edu>
2015-05-31 03:02:56 -0600

    added script that tests run_ir_ftn_mktest_lli's ability to deal with an optimized .ll file whose exit status indicates a problem.

---
+++ test/cases/run_ir_ftn_mktest_lli..o3_status.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# test run_ir_ftn_mktest_lli's ability to deal with an optimized .ll file
+# whose exit status indicates a problem.
+
+scriptname=<scriptname>
+
+if [ -z "TESTDRIVER_CASE_WORKDIR" ]; then
+   export TESTDRIVER_CASE_WORKDIR=tmp/${scriptname}_`date +Y%b%d_%H%M%S`_$$
+   mkdir $TESTDRIVER_CASE_WORKDIR
+   echo "had to create TESTDRIVER_CASE_WORKDIR="
+   echo "  " \"$TESTDRIVER_CASE_WORKDIR\"
+fi
+
+# -----------------------------------------------------------------------------
+# main script
+
+run_ir_ftn_mktest_lli -d $TESTDRIVER_CASE_WORKDIR --o3-script false \
+      test/data/func-3-2/001.ll
+
+(cd $TESTDRIVER_CASE_WORKDIR/001.ll; cat *.report)
+
+
+# -----------------------------------------------------------------------------
+# end of script
+
