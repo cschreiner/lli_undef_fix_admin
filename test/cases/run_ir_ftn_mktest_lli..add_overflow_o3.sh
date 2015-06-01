01b9d77bbde35e443603b7190c40d946c514e174
Christian Schreiner <cas@cs.utah.edu>
2015-06-01 17:23:43 -0600

    fixed syntax, and a filename

--- test/cases/run_ir_ftn_mktest_lli..add_overflow_o3.sh
+++ test/cases/run_ir_ftn_mktest_lli..add_overflow_o3.sh
@@ -4,7 +4,7 @@
 
 scriptname=run_ir_ftn_mktest..add_overflow_03.sh
 
-if [ -z "TESTDRIVER_CASE_WORKDIR" ]; then
+if [ -z "$TESTDRIVER_CASE_WORKDIR" ]; then
    export TESTDRIVER_CASE_WORKDIR=tmp/${scriptname}_`date +Y%b%d_%H%M%S`_$$
    mkdir $TESTDRIVER_CASE_WORKDIR
    echo "had to create TESTDRIVER_CASE_WORKDIR="
@@ -16,7 +16,7 @@ fi
 
 run_ir_ftn_mktest_lli -d $TESTDRIVER_CASE_WORKDIR \
       --o3-ll data/ir_ftn_mktest/add_overflow.ll \
-      test/data/func-3-2/001.ll
+      data/funcs-3-2/001.ll
 
 (cd $TESTDRIVER_CASE_WORKDIR/001.ll; cat *.report)
 
